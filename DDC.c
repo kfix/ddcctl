a79f544fec041d6ee3df505c6fb6b524a3aad331
Saman-VDR <saman.vdr@gmail.com>
2016-12-11 19:06:42 +0100

    Fix to read data from my Dell U2515H

--- DDC.c
+++ DDC.c
@@ -12,6 +12,7 @@
 #include <ApplicationServices/ApplicationServices.h>
 #include "DDC.h"
 #define kDelayBase 100
+#define kMaxRequests 100
 
 static io_service_t IOFramebufferPortFromCGDisplayID(CGDirectDisplayID displayID)
 //  iterate IOreg's device tree to find the IOFramebuffer mach service port that corresponds to a given CGDisplayID
@@ -177,8 +178,7 @@ bool DDCWrite(CGDirectDisplayID displayID, struct DDCWriteCommand *write) {
     data[5] = write->new_value & 255;
     data[6] = 0x6E ^ data[0] ^ data[1] ^ data[2] ^ data[3]^ data[4] ^ data[5];
 
-
-    request.replyTransactionType    = kIOI2CNoTransactionType;
+    request.replyTransactionType            = kIOI2CNoTransactionType;
     request.replyBytes                      = 0;
 
 
@@ -187,38 +187,44 @@ bool DDCWrite(CGDirectDisplayID displayID, struct DDCWriteCommand *write) {
 }
 
 bool DDCRead(CGDirectDisplayID displayID, struct DDCReadCommand *read) {
-    kern_return_t kr;
     IOI2CRequest request;
     UInt8 reply_data[11] = {};
     bool result = false;
     UInt8 data[128];
 
-
-    bzero( &request, sizeof(request));
-
-    request.commFlags                       = 0;
-
-    request.sendAddress                     = 0x6E;
-    request.sendTransactionType             = kIOI2CSimpleTransactionType;
-    request.sendBuffer                      = (vm_address_t) &data[0];
-    request.sendBytes                       = 5;
-    request.minReplyDelay                   = 0;
-
-    data[0] = 0x51;
-    data[1] = 0x82;
-    data[2] = 0x01;
-    data[3] = read->control_id;
-    data[4] = 0x6E ^ data[0] ^ data[1] ^ data[2] ^ data[3];
-
-    request.replyTransactionType    = kIOI2CDDCciReplyTransactionType;
-    request.replyAddress            = 0x6F;
-    request.replySubAddress         = 0x51;
-
-    request.replyBuffer = (vm_address_t) reply_data;
-    request.replyBytes = sizeof(reply_data);
-
-    result = DisplayRequest(displayID, &request);
-    result = (result && reply_data[0] == request.sendAddress && reply_data[2] == 0x2 && reply_data[4] == read->control_id && reply_data[10] == (request.replyAddress ^ request.replySubAddress ^ reply_data[1] ^ reply_data[2] ^ reply_data[3] ^ reply_data[4] ^ reply_data[5] ^ reply_data[6] ^ reply_data[7] ^ reply_data[8] ^ reply_data[9]));
+    for (int i=0; i<kMaxRequests; i++) {
+        bzero(&request, sizeof(request));
+        
+        request.commFlags                       = 0;   
+        request.sendAddress                     = 0x6E;
+        request.sendTransactionType             = kIOI2CSimpleTransactionType;
+        request.sendBuffer                      = (vm_address_t) &data[0];
+        request.sendBytes                       = 5;
+        //request.minReplyDelay                   = kDelayBase;
+        
+        data[0] = 0x51;
+        data[1] = 0x82;
+        data[2] = 0x01;
+        data[3] = read->control_id;
+        data[4] = 0x6E ^ data[0] ^ data[1] ^ data[2] ^ data[3];
+        
+        //request.replyTransactionType    = kIOI2CDDCciReplyTransactionType;
+        request.replyTransactionType    = kIOI2CSimpleTransactionType;
+        request.replyAddress            = 0x6F;
+        request.replySubAddress         = 0x51;
+        
+        request.replyBuffer = (vm_address_t) reply_data;
+        request.replyBytes = sizeof(reply_data);
+        
+        result = DisplayRequest(displayID, &request);
+        result = (result && reply_data[0] == request.sendAddress && reply_data[2] == 0x2 && reply_data[4] == read->control_id && reply_data[10] == (request.replyAddress ^ request.replySubAddress ^ reply_data[1] ^ reply_data[2] ^ reply_data[3] ^ reply_data[4] ^ reply_data[5] ^ reply_data[6] ^ reply_data[7] ^ reply_data[8] ^ reply_data[9]));
+    
+        if (result) {
+            printf("Tries required to get data: %d ", i);
+            printf("- Read address was %d  \n", reply_data[4]);
+            break;
+        }
+    }
     read->max_value = reply_data[7];
     read->current_value = reply_data[9];
     return result;
